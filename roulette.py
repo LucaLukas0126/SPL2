667dfa8b71911c1e80815bd51aa1a2597cfeb1fd
lucalukas0126 <171wita17@o365.htl-leoben.at>
2018-06-11 07:14:24 -0700

    Python Teil2

--- roulette.py
+++ roulette.py
@@ -4,8 +4,8 @@ import random
 zahlen = []
 anzahlRoulette = input("Wie oft soll es sich drehen?")
 anzahlRoulette = int(anzahlRoulette)
-for i in range (0,37):
-    wurf = random.randint(1,6)
+for i in range (1,37):
+    wurf = random.randint(1,36)
     zahlen.append(wurf)
     print(wurf, end="...")
 
@@ -13,5 +13,5 @@ print()
 print ("Ergebnis: ")
 print (zahlen)
 
-for i in range(1,7):
-print(i,"er: ",zahlen.count(1))
+for i in range(1,36):
+    print(i,"er: ",zahlen.count(i))
